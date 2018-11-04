742df4d61f4bac6d5bce1d0f52bab2a41dad16b1
lenastar <lena.starkova.1998@mail.ru>
2018-11-05 00:24:57 +0300

    First solution

--- emitter.js
+++ emitter.js
@@ -6,11 +6,39 @@
  */
 const isStar = true;
 
+class Event {
+    constructor(context, handler, expression) {
+        this.context = context;
+        this.handler = handler;
+        this.count = 0;
+        this.isAllowed = expression;
+    }
+}
+
+function parseEvent(event) {
+    let events = [event];
+    if (event.includes('.')) {
+        events.push(event.split('.')[0]);
+    }
+
+    return events;
+}
+
+
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    let events = new Map();
+    function subscribe(event, context, handler, expression) {
+        if (!events.has(event)) {
+            events.set(event, []);
+        }
+        let _event = new Event(context, handler, expression);
+        events.get(event).push(_event);
+    }
+
     return {
 
         /**
@@ -18,26 +46,54 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} this
          */
         on: function (event, context, handler) {
             console.info(event, context, handler);
+            subscribe(event, context, handler, () => true);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} this
          */
         off: function (event, context) {
             console.info(event, context);
+            let subscribes = Array.from(events.keys())
+                .filter(x => x.includes(event));
+            subscribes.forEach(el =>{
+                if (events.has(el)) {
+                    events.set(el, events.get(el).filter(x => x.context !== context));
+                }
+            });
+
+            return this;
+
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} this
          */
         emit: function (event) {
-            console.info(event);
+            let _events = parseEvent(event);
+            for (let i of _events) {
+                if (events.has(i)) {
+                    events.get(i).forEach(x => {
+                        if (x.isAllowed(x.count)) {
+                            x.handler.call(x.context);
+                        }
+                        x.count += 1;
+                    });
+                }
+            }
+
+            return this;
         },
 
         /**
@@ -47,9 +103,15 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object} this
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            if (times < 1) {
+                this.on(event, context, handler);
+            }
+            subscribe(event, context, handler, count => count < times);
+
+            return this;
         },
 
         /**
@@ -59,9 +121,16 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object} this
          */
         through: function (event, context, handler, frequency) {
             console.info(event, context, handler, frequency);
+            if (frequency < 1) {
+                this.on(event, context, handler);
+            }
+            subscribe(event, context, handler, count => count % frequency === 0);
+
+            return this;
         }
     };
 }
